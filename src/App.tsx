eebe23aea6131493b1e0207693faeb06c610d97c
Padunk <abraham.agel@gmail.com>
2018-07-26 11:03:33 +0700

    add animation with react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { CSSTransition} from 'react-transition-group';
+import TransitionGroup from 'react-transition-group/TransitionGroup';
 
 import { AddBook } from './components/Home/addBook';
 import { BookList } from './components/Home/bookList';
@@ -13,22 +15,44 @@ import { NotFound } from "./NotFound";
 
 import './App.css';
 
-class App extends React.Component<{}, {}> {
+interface IAppProp {
+  location: {
+    key: any;
+  }
+}
+
+class App extends React.Component<IAppProp, {}> {
+  constructor(props:IAppProp) {
+    super(props);
+  }
+
   public render() {
     return (
       <main id="main__app">
       <NavBar />
       <div className="app">
-        <Switch>
-        <Route exact={true} path='/' component = { BookList } />
-          <Route exact={true} path='/reader/' component = { MainReader } />
-          <Route exact={true} path='/reader/:book/:chapter' component= { Reader } />
-          <Route exact={true} path='/book/:detail' component = { BookListDetail } />
-          <Route exact={true} path='/newbook/add' component = { AddBook } />
-          <Route exact={true} path='/editbook/:title' component = { EditBook } />
-          <Route exact={true} path='/deletebook/:title' component = { DeleteBook } />
-          <Route component = { NotFound } />
-        </Switch>
+        <TransitionGroup className='app__transGroup'>
+          <CSSTransition
+            key={this.props.location.key}
+            classNames='fade'
+            timeout={{ enter:650, exit: 300}}
+            appear={true}
+            in={true}
+            mountOnEnter={true}
+            unmountOnExit={true}
+          >
+            <Switch location={this.props.location}>
+              <Route exact={true} path='/' component = { BookList } />
+              <Route exact={true} path='/reader/' component = { MainReader } />
+              <Route exact={true} path='/reader/:book/:chapter' component= { Reader } />
+              <Route exact={true} path='/book/:detail' component = { BookListDetail } />
+              <Route exact={true} path='/newbook/add' component = { AddBook } />
+              <Route exact={true} path='/editbook/:title' component = { EditBook } />
+              <Route exact={true} path='/deletebook/:title' component = { DeleteBook } />
+              <Route component = { NotFound } />
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
       </div>
       </main>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './App';
 
@@ -9,8 +9,12 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    {/*tslint:disable-next-line jsx-no-lambda*/}
+    <Route render={ ({ location }:{[key:string]:any}) => (
+      <App location={location} />
+    )} />
   </BrowserRouter>,
   document.getElementById('root') as HTMLElement
 );
+
 registerServiceWorker();
