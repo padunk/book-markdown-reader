bdc9eee2724fc31b6347ee49a8d344f992a76fd9
Padunk <abraham.agel@gmail.com>
2018-07-27 14:40:08 +0700

    make animation on navbar

--- src/components/Navigation/navbar.tsx
+++ src/components/Navigation/navbar.tsx
@@ -3,12 +3,46 @@ import { NavLink } from 'react-router-dom';
 
 import './navbar.css';
 
-export const NavBar: React.StatelessComponent<{}> = () => {
-    return (
-        <nav className='navbar__main'>
-            <NavLink exact={true} to='/' activeClassName='active'>Home</NavLink>
-            <NavLink to='/newbook/add'>Add Book</NavLink>
-            <NavLink to='/reader/'>Reader</NavLink>
-        </nav>
-    )
+export class NavBar extends React.Component<{}, {}> {
+    public componentDidMount() {
+        document.addEventListener('scroll', this.handleScroll, true);
+    }
+
+    public componentWillUnmount() {
+        document.removeEventListener('scroll', this.handleScroll);
+    }
+
+    public handleScroll = (e:any) => {
+        e.preventDefault();
+        
+        const userWidth = window.innerWidth;
+        // tslint:disable-next-line:no-console
+        // console.log(userWidth);
+        const nav = document.getElementById('navbar');
+        const positionY:number = e.srcElement.scrollingElement.scrollTop;
+
+        if (userWidth > 585){
+            if (positionY <= 200) {            
+                nav!.classList.remove('hideNavbar');
+                nav!.classList.remove('navbar__main--leftSide');
+                nav!.classList.add('navbar__main--topSide');
+            } else if (positionY >= 250 && positionY <= 300) {
+                nav!.classList.add('hideNavbar');
+            } else {
+                nav!.classList.remove('hideNavbar');
+                nav!.classList.remove('navbar__main--topSide');
+                nav!.classList.add('navbar__main--leftSide');
+            }
+        }
+    }
+
+    public render () {
+        return (
+            <nav className='navbar__main navbar__main--topSide' id='navbar' onScroll={this.handleScroll}>
+                <NavLink exact={true} to='/' activeClassName='active'>Home</NavLink>
+                <NavLink to='/newbook/add'>Add Book</NavLink>
+                <NavLink to='/reader/'>Reader</NavLink>
+            </nav>
+        );
+    }
 }
