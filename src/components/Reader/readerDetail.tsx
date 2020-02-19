f027e466eaa096d70ce41e1bc6abb1cd47a11a41
Padunk <abraham.agel@gmail.com>
2020-02-19 09:29:27 +0700

    feat(chapter): fixed chapter controller

--- src/components/Reader/readerDetail.tsx
+++ src/components/Reader/readerDetail.tsx
@@ -2,13 +2,19 @@ import insane from 'insane';
 import marked from 'marked';
 import Prism from 'prismjs';
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 import './reader.css';
 
 import { fetchChapter } from '../api/fetchingAPI';
-import { CHAPTER_REGEX } from '../utils/helper';
+import { CHAPTER_REGEX, checkChapter } from '../utils/helper';
 
 interface IReaderProps {
+    match: {
+        params: {
+            book: string;
+        };
+    };
     location: {
         state: {
             ch_digit: number;
@@ -39,8 +45,6 @@ export class Reader extends React.Component<IReaderProps, IReaderState> {
     }
 
     public componentDidMount() {
-        setTimeout(() => Prism.highlightAll(), 0);
-
         const { ch_digit, ch_end, ch_start, mdURL } = this.props.location.state;
 
         const presentChapter: number = Number(mdURL.match(CHAPTER_REGEX)![0]);
@@ -64,17 +68,28 @@ export class Reader extends React.Component<IReaderProps, IReaderState> {
         });
 
         this.fetchData(this.props.location.state.mdURL);
+        setTimeout(() => Prism.highlightAll(), 0);
+    }
+
+    public componentDidUpdate() {
+        setTimeout(() => Prism.highlightAll(), 0);
     }
 
     public parseMD = (str: string) => {
+        const renderer: any = new marked.Renderer();
         marked.setOptions({
             gfm: true,
-            highlight(code: string, lang: string) {
-                return Prism.highlight(code, Prism.languages[lang || 'markup']);
-            },
+            // highlight(code: string, lang: string) {
+            //     const language = lang === 'txt' ? 'markup' : lang;
+            //     return Prism.highlight(code, Prism.languages[language || 'js']);
+            // },
             sanitizer: insane,
         });
-        return { __html: marked(str) };
+
+        // const sanitizeString = insane(str, {
+        //     allowedTags: ['img', 'p']
+        // })
+        return { __html: marked(str, { renderer }) };
     };
 
     public fetchData = (url: string) => {
@@ -83,53 +98,63 @@ export class Reader extends React.Component<IReaderProps, IReaderState> {
         });
     };
 
-    public prevChapter = () => {
-        const newURL = this.props.location.state.mdURL.replace(
-            CHAPTER_REGEX,
-            String(this.state.prevChapter)
-        );
-
-        this.fetchData(newURL);
-        return undefined;
-    };
-
-    public nextChapter = () => {
-        const newURL = this.props.location.state.mdURL.replace(
-            CHAPTER_REGEX,
-            String(this.state.nextChapter)
-        );
-
-        this.fetchData(newURL);
-        return undefined;
-    };
-
     public render() {
-        const { ch_end, ch_start } = this.props.location.state;
-        const { presentChapter } = this.state;
         // ts-lint:disable-next-line:no-console
+        // console.log(this.props.location)
+        const { ch_digit, ch_end, ch_start, mdURL } = this.props.location.state;
+        const { data, nextChapter, presentChapter, prevChapter } = this.state;
 
+        const nextURL = checkChapter(ch_digit, mdURL, nextChapter);
+        const prevURL = checkChapter(ch_digit, mdURL, prevChapter);
+        const { book } = this.props.match.params;
+
+        // ts-lint:disable-next-line:no-console
+        // console.log('md:', mdURL);
+        // console.log('next:', nextURL);
         return (
             <div className='readerDetail__main'>
-                {this.state.data && (
+                {data && (
                     <div
                         id='reader__article'
-                        dangerouslySetInnerHTML={this.parseMD(this.state.data)}
+                        dangerouslySetInnerHTML={this.parseMD(data)}
                     />
                 )}
+                <div className='readerDetail__main--controller'>
                 {presentChapter === ch_start ? null : (
-                    <button
-                        className='reader__article--buttonPrev'
-                        onClick={this.prevChapter}>
-                        prev
-                    </button>
+                    <Link
+                        className="link__button"
+                        to={{
+                            pathname: `/reader/${book}/chapter${String(
+                                prevChapter
+                            )}`,
+                            state: {
+                                ch_digit,
+                                ch_end,
+                                ch_start,
+                                mdURL: prevURL,
+                            },
+                        }}>
+                        &lt; Chapter {String(prevChapter)}
+                    </Link>
                 )}
                 {presentChapter === ch_end ? null : (
-                    <button
-                        className='reader__article--buttonNext'
-                        onClick={this.nextChapter}>
-                        next
-                    </button>
+                    <Link
+                        className="link__button"
+                        to={{
+                            pathname: `/reader/${book}/chapter${String(
+                                nextChapter
+                            )}`,
+                            state: {
+                                ch_digit,
+                                ch_end,
+                                ch_start,
+                                mdURL: nextURL,
+                            },
+                        }}>
+                        Chapter {String(nextChapter)} &gt;
+                    </Link>
                 )}
+                </div>
             </div>
         );
     }
